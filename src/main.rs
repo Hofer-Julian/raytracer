d84dfed8b061cda72fcfcc2685fabf3f39a267e1
Julian Hofer <julianhofer@gnome.org>
2022-07-24 10:59:12 +0200

    Finish chapter 11.33

--- src/main.rs
+++ src/main.rs
@@ -54,9 +54,9 @@ fn main() {
     // World
     let mut world = World::new();
     let material_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Arc::new(Dielectric::new(1.5));
+    let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let material_left = Arc::new(Dielectric::new(1.5));
-    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground);
     let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center);
--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::{color::Color, hit::HitRecord, ray::Ray};
 
 use super::Scatter;
@@ -12,6 +14,12 @@ impl Dielectric {
             index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Scatter for Dielectric {
@@ -23,8 +31,20 @@ impl Scatter for Dielectric {
         };
 
         let unit_direction = r_in.direction().normalized();
-        let refracted = unit_direction.refract(rec.normal, refraction_ratio);
-        let scattered = Ray::new(rec.point, refracted);
+        let cos_theta = ((-1.0) * unit_direction).dot(rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let mut rng = rand::thread_rng();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let will_reflect = rng.gen::<f64>() < Self::reflectance(cos_theta, refraction_ratio);
+
+        let direction = if cannot_refract || will_reflect {
+            unit_direction.reflect(rec.normal)
+        } else {
+            unit_direction.refract(rec.normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(rec.point, direction);
 
         Some((Color::new(1.0, 1.0, 1.0), scattered))
     }
